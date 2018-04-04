a52121cf374ccb59fffa81586b181ed5134d5cc8
ramiro <liko28s@gmail.com>
2018-04-04 14:03:46 -0500

    Card Model and Migration

---
+++ models/card.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Card = sequelize.define('Card', {
+    uid: DataTypes.STRING,
+    active: DataTypes.BOOLEAN,
+    keyA: DataTypes.STRING,
+    keyB: DataTypes.STRING
+  }, {});
+  Card.associate = function(models) {
+    // associations can be defined here
+  };
+  return Card;
+};
